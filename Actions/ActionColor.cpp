2e786377be79a833bde88cfea9df5417b9d8fe2e
Essam <essamelsharkawy88@gmail.com>
2023-01-08 08:08:35 +0200

    color

--- Actions/ActionColor.cpp
+++ Actions/ActionColor.cpp
@@ -13,15 +13,19 @@ void ActionColor::Execute()
 {
 	Point P1;
 	color newColor;
+	bool flag = true;
 
 	//Get a Pointer to the Interface
 	GUI* pGUI = pManager->GetGUI();
-	
-	//Read 1st point to pick border color
-	pGUI->PrintMessage("Pick Color: Click at Color");
-	//Get 1st point and store it in color
-	pGUI->GetPointClicked(P1.x, P1.y);
-	newColor = pGUI->pWind->GetColor(P1.x, P1.y);
+	do {
+		//Read 1st point to pick border color
+		pGUI->PrintMessage("Pick Color: Click at Color");
+		//Get 1st point and store it in color
+		pGUI->GetPointClicked(P1.x, P1.y);
+		newColor = pGUI->pWind->GetColor(P1.x, P1.y);
+		if (newColor == BLACK || newColor == HOTRED || newColor == WHITE || newColor == HOTBLUE || newColor == HOTGREEN || newColor == HOTYELLOW)
+			flag = false;
+	} while (flag);
 	
 	//check if there is a figure selected to change border color
 	CFigure* fig = pManager->GetSelectedFigure();
